0f882ae2f1ccf4d0283f815927d4e320e78fff3c
totetero <totetero@gmail.com>
2017-08-30 08:59:40 +0900

    つぶやきと表示

--- rails-5.1.3/test_app/app/controllers/users_controller.rb
+++ rails-5.1.3/test_app/app/controllers/users_controller.rb
@@ -9,8 +9,17 @@ class UsersController < ApplicationController
       @user[:name] = 'none'
       @user[:username] = 'none'
     end
+    @tweets = Tweet.all
   end
 
   def new
+  end
+
+  def create
+    @tweet = Tweet.new
+    @tweet.title = params[:tweet][:title]
+    @tweet.content = params[:tweet][:content]
+    @tweet.save
+    redirect_to '/users/index'
   end
 end
--- rails-5.1.3/test_app/config/routes.rb
+++ rails-5.1.3/test_app/config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
 
   get "users/show/:username" => "users#show"
 
+  post "tweets" => "users#create"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
