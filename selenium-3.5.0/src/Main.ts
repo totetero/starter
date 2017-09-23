fa8c915a5205394cf6b73cbec2ba3fd7b85d4bdd
totetero <totetero@gmail.com>
2017-09-23 18:51:56 +0900

    少しコード修正

--- selenium-3.5.0/src/Main.ts
+++ selenium-3.5.0/src/Main.ts
@@ -5,15 +5,45 @@ import * as webdriver from "selenium-webdriver";
 // ----------------------------------------------------------------
 
 class Main{
-	// コンストラクタ
-	constructor(){}
+	private _driver: webdriver.ThenableWebDriver;
+	private _isStart: boolean = false;
 
-	// サーバ起動
-	public start(): void{
+	// コンストラクタ
+	constructor(){
+		// ドライバ作成
 		const builder: webdriver.Builder = new webdriver.Builder().forBrowser("chrome");
-		const driver: webdriver.ThenableWebDriver = builder.build();
-		driver.get("http://totetero.com").then((): void => {
-			driver.quit();
+		this._driver = builder.build();
+
+		// ブラウザを開く
+		this._driver.get("https://google.com").then((): void => {
+			this._isStart = true;
+		});
+	}
+
+	// 起動
+	public start(): void{
+		const waitConditionMessageOpen: string = "start up browser";
+
+		Promise.resolve().then((): webdriver.promise.Promise<any> => {
+			// ブラウザが起動するのを待ってから処理を開始する
+			return this._driver.wait(new webdriver.Condition(waitConditionMessageOpen, (): boolean => this._isStart), 10000);
+		}).then((): Promise<void> => new Promise<void>((resolve: ()=>void, reject: (err: any)=>void): void => {
+			// とりあえず少し待つ
+			setTimeout((): void => resolve(), 1000);
+		})).then((): void => {
+			// 完了したので終了する
+			this._driver.quit();
+		}).catch((err: any): void => {
+			// エラーハンドリング
+			if(err instanceof webdriver.error.TimeoutError && err.message.indexOf(waitConditionMessageOpen) >= 0){
+				// ブラウザを開くエラー
+				console.error("timeout error open", err);
+			}else{
+				// その他のエラー
+				console.error("unknown error", err);
+			}
+			// エラーなので終了する
+			this._driver.quit();
 		});
 	}
 }
