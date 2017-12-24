a00f482c82fcb32424dcccdc9ccb45150b605ddc
totetero <totetero@gmail.com>
2017-12-24 22:58:52 +0900

    elixir phoenix docker

--- elixir_phoenix-1.2.5/docker.sh
+++ elixir_phoenix-1.2.5/docker.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 [ $# -eq 0 ] && sh $0 help && exit
-[ $# -eq 1 ] && [ $1 = "setup" ] && sh $0 create start install put && exit
+[ $# -eq 1 ] && [ $1 = "first" ] && sh $0 create start install put setup && exit
+[ $# -eq 1 ] && [ $1 = "last" ] && sh $0 stop clear && exit
 
 DOCKER_CONTAINER_NAME_01=docker-starter-phoenix-main
 DOCKER_ELIXIR=elixir:1.5.3-slim
@@ -31,12 +32,14 @@ for ARG in "$@" ; do
 			docker rm ${DOCKER_CONTAINER_NAME_01}
 			;;
 		install)
-			echo -------- install init --------
+			echo -------- install apt-get --------
 			docker exec -i -t ${DOCKER_CONTAINER_NAME_01} /bin/bash -c 'apt-get update'
-			echo -------- install rsync --------
-			docker exec -i -t ${DOCKER_CONTAINER_NAME_01} /bin/bash -c 'DEBIAN_FRONTEND=noninteractive apt-get install -y rsync'
+			docker exec -i -t ${DOCKER_CONTAINER_NAME_01} /bin/bash -c 'DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends rsync'
+			docker exec -i -t ${DOCKER_CONTAINER_NAME_01} /bin/bash -c 'DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends nodejs npm'
+			docker exec -i -t ${DOCKER_CONTAINER_NAME_01} /bin/bash -c 'DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends mysql-client'
 			echo -------- install mix --------
 			docker exec -i -t ${DOCKER_CONTAINER_NAME_01} /bin/bash -c 'yes | mix local.hex'
+			docker exec -i -t ${DOCKER_CONTAINER_NAME_01} /bin/bash -c 'yes | mix local.rebar'
 			docker exec -i -t ${DOCKER_CONTAINER_NAME_01} /bin/bash -c 'yes | mix archive.install '${GIT_PHOENIX}
 			echo -------- finish install --------
 			;;
@@ -50,8 +53,25 @@ for ARG in "$@" ; do
 			rsync --blocking-io -e 'docker exec -i' -rltDv ${DOCKER_CONTAINER_NAME_01}:/root/${PROJECT}/ ${PROJECT}/
 			echo -------- finish sync get --------
 			;;
+		setup)
+			echo -------- setup --------
+			docker exec -i -t ${DOCKER_CONTAINER_NAME_01} bash -c 'cd /root/'${PROJECT}' && mix deps.get'
+			docker exec -i -t ${DOCKER_CONTAINER_NAME_01} bash -c 'cd /root/'${PROJECT}' && npm install'
+			docker exec -i -t ${DOCKER_CONTAINER_NAME_01} bash -c 'cd /root/'${PROJECT}' && npm install ./deps/phoenix'
+			docker exec -i -t ${DOCKER_CONTAINER_NAME_01} bash -c 'cd /root/'${PROJECT}' && npm install ./deps/phoenix_html'
+			echo -------- finish setup --------
+			;;
+		serve)
+			docker exec -i -t ${DOCKER_CONTAINER_NAME_01} bash -c 'cd /root/'${PROJECT}' && mix phoenix.server'
+			;;
+		browse)
+			DOCKER_IP=$(docker inspect -f '{{.NetworkSettings.IPAddress}}' ${DOCKER_CONTAINER_NAME_01})
+			DOCKER_HOSTIP=$(docker inspect -f '{{(index (index .NetworkSettings.Ports "4000/tcp") 0).HostIp}}' ${DOCKER_CONTAINER_NAME_01})
+			DOCKER_HOSTPORT=$(docker inspect -f '{{(index (index .NetworkSettings.Ports "4000/tcp") 0).HostPort}}' ${DOCKER_CONTAINER_NAME_01})
+			open http://${DOCKER_HOSTIP}:${DOCKER_HOSTPORT}
+			;;
 		help)
-			echo create or status or start or install or put or get or bash or stop or clear
+			echo create or status or start or install or put or get or setup or serve or browse or bash or stop or clear
 			;;
 	esac
 done
