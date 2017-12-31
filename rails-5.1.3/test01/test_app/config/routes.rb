3b60f9041b02e93991cd96266ac3fbeb64727627
totetero <totetero@gmail.com>
2018-01-01 02:49:36 +0900

    rails renew

--- rails-5.1.3/test01/test_app/config/routes.rb
+++ rails-5.1.3/test01/test_app/config/routes.rb
@@ -1,11 +1,3 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  get 'users/index'
-
-  get "users/show/:username" => "users#show"
-
-  post "tweets" => "users#create"
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- rails-5.1.3/test01/test_app/db/seeds.rb
+++ rails-5.1.3/test01/test_app/db/seeds.rb
@@ -5,18 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-@user = User.new
-@user.name = 'hoge1'
-@user.username = 'hoge2'
-@user.location = 'hoge3'
-@user.about = 'hoge4'
-@user.save
-
-@user = User.new
-@user.name = 'fuga1'
-@user.username = 'fuga2'
-@user.location = 'fuga3'
-@user.about = 'fuga4'
-@user.save
-
