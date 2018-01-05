ff13e862eecb6234ffd6cc5d1aa279a5a195ea5c
totetero <totetero@gmail.com>
2018-01-05 23:48:13 +0900

    phoenix route

--- elixir_phoenix-1.2.5/test02/web/router.ex
+++ elixir_phoenix-1.2.5/test02/web/router.ex
@@ -17,6 +17,7 @@ defmodule Test02.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    resources "/players", PlayerController
   end
 
   # Other scopes may use custom stacks.
