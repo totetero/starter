85d30bee3b9945c957bdc40997d995b40bcf0152
totetero <totetero@gmail.com>
2018-01-08 01:25:42 +0900

    tensorflow-1.4.1 rsync make

--- tensorflow-1.4.1/docker.sh
+++ tensorflow-1.4.1/docker.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 [ $# -eq 0 ] && sh $0 help && exit
-[ $# -eq 1 ] && [ $1 = "first" ] && sh $0 create start && exit
+[ $# -eq 1 ] && [ $1 = "first" ] && sh $0 create start put && exit
 [ $# -eq 1 ] && [ $1 = "last" ] && sh $0 stop clear && exit
 
 DOCKER_CONTAINER_NAME_01=docker-starter-tensorflow-main
 DOCKER_TENSORFLOW=tensorflow/tensorflow:1.4.1
+PROJECT=test01
 
 for ARG in "$@" ; do
 	echo -------- $ARG start --------
@@ -30,8 +31,17 @@ for ARG in "$@" ; do
 		clear)
 			docker rm ${DOCKER_CONTAINER_NAME_01}
 			;;
+		make)
+			docker exec -it ${DOCKER_CONTAINER_NAME_01} /bin/bash -c 'cd /root/'${PROJECT}' && make'
+			;;
+		sync_put|put)
+			rsync --blocking-io -e 'docker exec -i' --exclude='.git' --filter=':- .gitignore' -rltDv ${PROJECT}/ ${DOCKER_CONTAINER_NAME_01}:/root/${PROJECT}/
+			;;
+		sync_get|get)
+			rsync --blocking-io -e 'docker exec -i' --exclude='.git' --filter=':- .gitignore' -rltDv ${DOCKER_CONTAINER_NAME_01}:/root/${PROJECT}/ ${PROJECT}/
+			;;
 		help)
-			echo create or status or start or bash or stop or clear
+			echo create or status or start or put or get or bash or make or stop or clear
 			;;
 		*)
 			echo nothing to do
