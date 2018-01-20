da83bef891ef33ad724e217beb2cbab0658d44a8
totetero <totetero@gmail.com>
2018-01-20 23:26:09 +0900

    ファイル名変更

--- tensorflow-1.4.1/test01/test002.py
+++ tensorflow-1.4.1/test01/test002.py
@@ -1,30 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Qiita 多分もっともわかりやすいTensorFlow 入門 (Introduction)
-# https://qiita.com/junichiro/items/8886f3976fc20f73335f
-# テンソルフローのサンプルである線形回帰を計算する
+# Qiita 機械学習ライブラリTensorFlowで学習しない〜フィボナッチ数列
+# https://qiita.com/n_kats_/items/252a4cea0dc8eca8f980
+# 普通のフィボナッチ
 
 import tensorflow as tf
-import numpy as np
 
 # ----------------------------------------------------------------
 # ----------------------------------------------------------------
 # ----------------------------------------------------------------
 # 初期化フェイズ
 
-init_W = tf.random_uniform(shape=[1], minval=-1.0, maxval=1.0, dtype=tf.float32)
-init_b = tf.zeros(shape=[1], dtype=tf.float32)
-W = tf.Variable(initial_value=init_W, dtype=tf.float32)
-b = tf.Variable(initial_value=init_b, dtype=tf.float32)
-x = tf.placeholder(dtype=tf.float32)
-y = tf.placeholder(dtype=tf.float32)
+u = tf.Variable(initial_value=1, dtype=tf.int32)
+v = tf.Variable(initial_value=0, dtype=tf.int32)
 
-# 線形回帰のコスト関数の定義 y = x * W + b
-loss = tf.reduce_mean(tf.square((x * W + b) - y) / 2)
-# 学習率0.5で最急降下法を用いてコストを最小にする計算
-optimizer = tf.train.GradientDescentOptimizer(0.5)
-train = optimizer.minimize(loss)
+update_u = tf.assign(u, tf.add(u, v))
+update_v = tf.assign(v, tf.subtract(u, v))
 
 # 実行前の初期化
 init = tf.global_variables_initializer()
@@ -34,23 +26,22 @@ init = tf.global_variables_initializer()
 # ----------------------------------------------------------------
 # 実行フェイズ
 
-# y = x * 0.1 + 0.3
-# 入力データとして100個のグラフ上の点を用意する
-x_train = np.random.rand(100).astype(np.float32)
-y_train = x_train * 0.1 + 0.3
-
 # セッションを作って実行する
 with tf.Session() as sess:
 	sess.run(init)
-	temp_W, temb_b = sess.run([W, b])
-	print(0, temp_W, temb_b)
-	for i in range(200):
-		step = i + 1
-		sess.run(train, {x: x_train, y: y_train})
-		if step % 20 == 0:
-			temp_W, temb_b = sess.run([W, b])
-			print(step, temp_W, temb_b)
-	#tf.summary.FileWriter('./', sess.graph)
+	step = 1
+	test_u = 1
+	test_v = 0
+	temp_u = sess.run(u)
+	print(1, temp_u)
+	for i in range(99):
+		sess.run(update_u)
+		sess.run(update_v)
+		step = step + 1
+		test_u = test_u + test_v
+		test_v = test_u - test_v
+		temp_u = sess.run(u)
+		print(step, test_u, temp_u)
 
 # ----------------------------------------------------------------
 # ----------------------------------------------------------------
