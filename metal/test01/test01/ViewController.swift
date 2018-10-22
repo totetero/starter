5121312da85217821907e8c413506c6275620adb
totetero <totetero@gmail.com>
2018-10-23 01:19:37 +0900

    メタル確認用xcodeプロジェクト作成

---
+++ metal/test01/test01/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  test01
+//
+//  Created by totetero on 2018/10/23.
+//  Copyright © 2018 totetero. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
