ea22ccb18d2135520663254976ff8599af04512c
totetero <totetero@gmail.com>
2018-12-27 11:45:14 +0900

    バルカン確認用xcodeプロジェクト作成

---
+++ metal/test02/test02/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  test02
+//
+//  Created by totetero on 2018/12/27.
+//  Copyright © 2018 totetero. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
