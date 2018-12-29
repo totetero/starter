29f20e949c684350d9f17b3ed758a9ea21d28d37
totetero <totetero@gmail.com>
2018-12-29 21:11:54 +0900

    バルカン確認用xcodeプロジェクト作成

---
+++ metal/test02/test02/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test02
+//
+//  Created by totetero on 2018/12/29.
+//  Copyright © 2018 totetero. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
