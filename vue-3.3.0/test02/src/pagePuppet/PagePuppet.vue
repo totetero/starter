29b8046460e63cddf5a193098ccc3161c9bc3f76
totetero <totetero@gmail.com>
2019-02-07 00:13:53 +0900

    処理を分ける

--- vue-3.3.0/test02/src/pagePuppet/PagePuppet.vue
+++ vue-3.3.0/test02/src/pagePuppet/PagePuppet.vue
@@ -1,21 +1,11 @@
 <template>
 	<div>
-		<svg :viewbox="`0 0 ${this.w} ${this.h}`" :width="this.w" :height="this.h">
+		<svg :viewbox="`0 0 ${w} ${h}`" :width="w" :height="h">
+			<rect x="0" y="0" :width="w" :height="h" fill="red" />
 			<template v-for="puppet in puppets">
 				<ComponentPuppet :x="puppet.px" :y="puppet.py" :r="puppet.r" />
 			</template>
-			<rect :x="m" :y="m" :width="wm2" :height="hm2" stroke="black" stroke-width="1" fill="none" />
-			<line :x1="x0" :y1="y0" :x2="x1" :y2="y1" stroke="black" />
-			<line :x1="x1" :y1="y1" :x2="x2" :y2="y2" stroke="black" />
-			<line :x1="x2" :y1="y2" :x2="x3" :y2="y3" stroke="black" />
-			<line :x1="x3" :y1="y3" :x2="x0" :y2="y0" stroke="black" />
-			<circle :cx="x0" :cy="y0" :r="m" fill="#06f" />
-			<circle :cx="x1" :cy="y1" :r="m" fill="#06f" />
-			<circle :cx="x2" :cy="y2" :r="m" fill="#06f" />
-			<circle :cx="x3" :cy="y3" :r="m" fill="#06f" />
 		</svg>
-		<div><input type="range" min="0" max="100" v-model="rangeValue"></div>
-		<ComponentButton>あいうえお</ComponentButton>
 	</div>
 </template>
 
@@ -36,7 +26,6 @@
 		mutationMatrixSet,
 	} from './ModulePuppet';
 	import {mat4,} from 'gl-matrix';
-	import ComponentButton from '../partsCommon/ComponentButton.vue';
 	import ComponentPuppet from './ComponentPuppet.vue';
 
 	// ----------------------------------------------------------------
@@ -45,13 +34,10 @@
 
 	@Component({
 		components: {
-			ComponentButton,
 			ComponentPuppet,
 		},
 	})
 	export default class PagePuppet extends Vue{
-		private m: number = 10;
-		private rangeValue: string = '50';
 		private puppets: {
 			id: number;
 			zIndex: number;
@@ -64,19 +50,6 @@
 
 		private get w(): number{return this.$store.getters[getterWidth];}
 		private get h(): number{return this.$store.getters[getterHeight];}
-		private get rangeRatio(): number{return parseInt(this.rangeValue, 10) / 100;}
-		private get wm2(): number{return this.w - this.m * 2;}
-		private get hm2(): number{return this.h - this.m * 2;}
-		private get wm4(): number{return this.w - this.m * 4;}
-		private get hm4(): number{return this.h - this.m * 4;}
-		private get x0(): number{return this.m * 2 + this.wm4 * this.rangeRatio;}
-		private get y0(): number{return this.hm2;}
-		private get x1(): number{return this.wm2;}
-		private get y1(): number{return this.hm2 - this.hm4 * this.rangeRatio;}
-		private get x2(): number{return this.wm2 - this.wm4 * this.rangeRatio;}
-		private get y2(): number{return this.m * 2;}
-		private get x3(): number{return this.m * 2;}
-		private get y3(): number{return this.m * 2 + this.hm4 * this.rangeRatio;}
 
 		// 作成時処理
 		private created(): void{
@@ -114,9 +87,9 @@
 
 			// カメラ行列
 			const cr: number = 20;
-			const cx: number = cr * Math.sin(this.rangeRatio * Math.PI);
+			const cx: number = cr * Math.sin(0.2 * step * Math.PI / 180);
 			const cy: number = cr;
-			const cz: number = cr * Math.cos(this.rangeRatio * Math.PI); 
+			const cz: number = cr * Math.cos(0.2 * step * Math.PI / 180); 
 			mat4.lookAt(tempMat3, [cx, cy, cz], [0, 0, 0], [0, 1, 0]);
 			mat4.multiply(tempMat1, tempMat1, tempMat3);
 
--- vue-3.3.0/test02/src/pageVector/PageVector.vue
+++ vue-3.3.0/test02/src/pageVector/PageVector.vue
@@ -1,5 +1,19 @@
 <template>
-	<div>ベクターページ</div>
+	<div>
+		<svg :viewbox="`0 0 ${w} ${h}`" :width="w" :height="h">
+			<rect :x="m" :y="m" :width="wm2" :height="hm2" stroke="black" stroke-width="1" fill="none" />
+			<line :x1="x0" :y1="y0" :x2="x1" :y2="y1" stroke="black" />
+			<line :x1="x1" :y1="y1" :x2="x2" :y2="y2" stroke="black" />
+			<line :x1="x2" :y1="y2" :x2="x3" :y2="y3" stroke="black" />
+			<line :x1="x3" :y1="y3" :x2="x0" :y2="y0" stroke="black" />
+			<circle :cx="x0" :cy="y0" :r="m" fill="#06f" />
+			<circle :cx="x1" :cy="y1" :r="m" fill="#06f" />
+			<circle :cx="x2" :cy="y2" :r="m" fill="#06f" />
+			<circle :cx="x3" :cy="y3" :r="m" fill="#06f" />
+		</svg>
+		<div><input type="range" min="0" max="100" v-model="rangeValue"></div>
+		<ComponentButton>あいうえお</ComponentButton>
+	</div>
 </template>
 
 <script lang="ts">
@@ -8,15 +22,37 @@
 	// ----------------------------------------------------------------
 
 	import {Component, Vue,} from 'vue-property-decorator';
+	import ComponentButton from '../partsCommon/ComponentButton.vue';
 
 	// ----------------------------------------------------------------
 	// ----------------------------------------------------------------
 	// ----------------------------------------------------------------
 
 	@Component({
-		components: {},
+		components: {
+			ComponentButton,
+		},
 	})
-	export default class PageVector extends Vue{}
+	export default class PageVector extends Vue{
+		private w: number = 300;
+		private h: number = 300;
+		private m: number = 10;
+		private rangeValue: string = '50';
+
+		private get rangeRatio(): number{return parseInt(this.rangeValue, 10) / 100;}
+		private get wm2(): number{return this.w - this.m * 2;}
+		private get hm2(): number{return this.h - this.m * 2;}
+		private get wm4(): number{return this.w - this.m * 4;}
+		private get hm4(): number{return this.h - this.m * 4;}
+		private get x0(): number{return this.m * 2 + this.wm4 * this.rangeRatio;}
+		private get y0(): number{return this.hm2;}
+		private get x1(): number{return this.wm2;}
+		private get y1(): number{return this.hm2 - this.hm4 * this.rangeRatio;}
+		private get x2(): number{return this.wm2 - this.wm4 * this.rangeRatio;}
+		private get y2(): number{return this.m * 2;}
+		private get x3(): number{return this.m * 2;}
+		private get y3(): number{return this.m * 2 + this.hm4 * this.rangeRatio;}
+	}
 
 	// ----------------------------------------------------------------
 	// ----------------------------------------------------------------
