c9d698505cee213b3b33a73a20916d2db19cf392
totetero <totetero@gmail.com>
2019-06-18 00:14:53 +0900

    createrを使わないreact

---
+++ react-16.8.1/src/Main.ts
@@ -0,0 +1,19 @@
+
+// ----------------------------------------------------------------
+// ----------------------------------------------------------------
+// ----------------------------------------------------------------
+
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import ComponentMain from "./component/ComponentMain";
+
+
+// 処理はここから始まる
+document.addEventListener("DOMContentLoaded", (event: Event): void => {
+	ReactDOM.render(React.createElement(ComponentMain), document.getElementById("app"));
+});
+
+// ----------------------------------------------------------------
+// ----------------------------------------------------------------
+// ----------------------------------------------------------------
+
