1094c82759882d59437328879a98fb36524d174e
totetero <totetero@gmail.com>
2019-06-19 16:06:09 +0900

    useEffectをも使ってみる

--- react-16.8.1/src/component/ComponentPageCount.tsx
+++ react-16.8.1/src/component/ComponentPageCount.tsx
@@ -4,23 +4,53 @@
 // ----------------------------------------------------------------
 
 import * as React from "react";
+import {Link,} from "react-router-dom";
 
 const Component: React.FunctionComponent<{
-	countInit: number;
+	localCountInit: number;
 }> = ({
-	countInit = 10,
+	localCountInit = 10,
 }): JSX.Element => {
-	const [countCurr, setCount,]: [number, (countNext: number) => void,] = React.useState<number>(countInit);
+	// メインループ設定
+	setMainloop((): void => {
+		console.log("mainloop");
+	});
+
+	// ステート設定 ローカルカウント
+	const [localCountCurr, setLocalCount,]: [number, (localCountNext: number) => void,] = React.useState<number>(localCountInit);
+
 	return (
 		<div>
-			<div>Clicks: {countCurr}</div>
-			<button onClick={(): void => setCount(countCurr + 1)}>+</button>
-			<button onClick={(): void => setCount(countCurr - 1)}>-</button>
+			<div>
+				<button onClick={(): void => setLocalCount(localCountCurr + 1)}>+</button>
+				<button onClick={(): void => setLocalCount(localCountCurr - 1)}>-</button>
+				<span>ローカルカウント: {localCountCurr}</span>
+			</div>
+			<div><Link to='/'>top</Link></div>
 		</div>
 	);
 };
 
 export default Component;
+
+// ----------------------------------------------------------------
+// ----------------------------------------------------------------
+// ----------------------------------------------------------------
+
+// メインループ設定
+const setMainloop = (mainloop: () => void): void => {
+	React.useEffect((): () => void => {
+		let requestID: number = -1;
+		const callback: FrameRequestCallback = (time: number): void => {
+			mainloop();
+			requestID = window.requestAnimationFrame(callback);
+		};
+		callback(0);
+		return () => {
+			window.cancelAnimationFrame(requestID);
+		};
+	}, []);
+}
 
 // ----------------------------------------------------------------
 // ----------------------------------------------------------------
