0e4e4e90a32bfa736776815b3f31622bf4246551
totetero <totetero@gmail.com>
2019-07-04 09:50:38 +0900

    iOSアプリケーションプロジェクト作成

---
+++ native_framework/IosTestApp/IosTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IosTestApp
+//
+//  Created by totetero on 2019/07/04.
+//  Copyright © 2019 totetero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
