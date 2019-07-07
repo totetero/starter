6abe98b04d7251e99dd3438b15249d98faa7e83c
totetero <totetero@gmail.com>
2019-07-07 11:25:38 +0900

    戻るボタン

--- native_framework/AndTestLib/librarytest/src/main/java/com/totetero/test/librarytest/LibraryTestActivity.kt
+++ native_framework/AndTestLib/librarytest/src/main/java/com/totetero/test/librarytest/LibraryTestActivity.kt
@@ -2,11 +2,16 @@ package com.totetero.test.librarytest
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_library_test.*
 
 class LibraryTestActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_library_test)
+
+        libButton1.setOnClickListener {
+            finish()
+        }
     }
 }
