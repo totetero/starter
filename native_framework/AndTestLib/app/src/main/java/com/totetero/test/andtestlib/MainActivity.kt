81221c09a2f71ab01dafbb0c4d2a236835297d0e
totetero <totetero@gmail.com>
2019-07-07 11:05:39 +0900

    アクティビティ遷移

--- native_framework/AndTestLib/app/src/main/java/com/totetero/test/andtestlib/MainActivity.kt
+++ native_framework/AndTestLib/app/src/main/java/com/totetero/test/andtestlib/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.totetero.test.andtestlib
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.totetero.test.librarytest.LibraryTestActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        testButton1.setOnClickListener {
+            var intent = Intent(applicationContext, LibraryTestActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
