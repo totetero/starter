0835df1a18549851eb58d0e1b8ac8f3302285dc8
totetero <totetero@gmail.com>
2019-07-07 22:15:20 +0900

    ライブラリを呼び出してみる

--- native_framework/AndTestApp/app/src/main/java/com/totetero/test/andtestapp/MainActivity.kt
+++ native_framework/AndTestApp/app/src/main/java/com/totetero/test/andtestapp/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.totetero.test.andtestapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.totetero.test.librarytest.LibraryTestActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        testButton1.setOnClickListener {
+            finish()
+        }
+
+        testButton2.setOnClickListener {
+            var intent = Intent(applicationContext, LibraryTestActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
