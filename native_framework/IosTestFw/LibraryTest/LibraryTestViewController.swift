0ad34945d0816f6e6ae7591dd36abe8194c2f061
totetero <totetero@gmail.com>
2019-07-15 09:27:42 +0900

    ライブラリのuiが見えない不具合修正

--- native_framework/IosTestFw/LibraryTest/LibraryTestViewController.swift
+++ native_framework/IosTestFw/LibraryTest/LibraryTestViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 public class LibraryTestViewController: UIViewController {
 
+    public init() {
+        let bundle = Bundle(for: LibraryTestViewController.self)
+        super.init(nibName: "LibraryTestViewController", bundle: bundle)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     public override func viewDidLoad() {
         super.viewDidLoad()
 
