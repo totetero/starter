0476a3d04f3be515f14a4a8322aaa38257e6ea02
totetero <totetero@gmail.com>
2019-07-17 09:44:40 +0900

    ライブラリ差し替え

---
+++ native_framework/IosTestApp/Frameworks/LibraryTest.framework/Headers/LibraryTest.h
@@ -0,0 +1,19 @@
+//
+//  LibraryTest.h
+//  LibraryTest
+//
+//  Created by totetero on 2019/07/10.
+//  Copyright © 2019 totetero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LibraryTest.
+FOUNDATION_EXPORT double LibraryTestVersionNumber;
+
+//! Project version string for LibraryTest.
+FOUNDATION_EXPORT const unsigned char LibraryTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LibraryTest/PublicHeader.h>
+
+
