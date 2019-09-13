a56db830524814ab9fdaa3f5827f79ab6c791bc9
totetero <totetero@gmail.com>
2019-09-13 09:53:16 +0900

    とりあえずwebviewを追加

--- native_ios_webrtc/native_ios_webrtc/ViewController.swift
+++ native_ios_webrtc/native_ios_webrtc/ViewController.swift
@@ -7,14 +7,28 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    private var webView: WKWebView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let url: URL = URL(string: "https://google.com")!
+        let request: URLRequest = URLRequest(url: url)
+        self.webView = WKWebView(frame: CGRect.zero)
+        self.webView.load(request)
+        self.view.addSubview(webView)
     }
 
-
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let width: CGFloat = self.view.bounds.size.width
+        let height: CGFloat = self.view.bounds.size.height
+        let frame: CGRect = CGRect(x:0, y:0, width: width, height: height)
+        self.webView.frame = frame
+    }
 }
 
