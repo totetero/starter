a91d9543211ee411cc1ab7b6b7bb0af100fd4ce8
totetero <totetero@gmail.com>
2019-10-04 09:18:49 +0900

    プロジェクト作成

---
+++ native_ios_webview_camera/native_ios_webview_camera/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  native_ios_webview_camera
+//
+//  Created by totetero on 2019/10/04.
+//  Copyright © 2019 totetero. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
