d90a35e0defd8a047b358f577750561ea3059779
totetero <totetero@gmail.com>
2020-03-04 00:06:08 +0900

    ビルダー修正

--- rust_wasm/docker_builder.sh
+++ rust_wasm/docker_builder.sh
@@ -8,7 +8,8 @@
 DOCKER_CONTAINER_NAME_01=docker-fuhaha-node-rust
 DOCKER_IMAGE_LINUX=ubuntu
 DOCKER_IMAGE_LINUX_TAG=18.04
-PROJECT=${PWD##*/}
+PROJECT=/root/project/${PWD##*/}
+PROFILE=/root/project/profile.sh
 
 for ARG in "${@}" ; do
 	echo -------- ${ARG} start --------
@@ -42,10 +43,10 @@ for ARG in "${@}" ; do
 			;;
 		setup_global|setup)
 			echo -------------------------------- setup start --------------------------------
-			docker exec -it ${DOCKER_CONTAINER_NAME_01} /bin/bash -c 'mkdir $HOME/project'
-			docker exec -it ${DOCKER_CONTAINER_NAME_01} /bin/bash -c 'touch $HOME/project/set.sh'
-			docker exec -it ${DOCKER_CONTAINER_NAME_01} /bin/bash -c 'echo "#!/bin/bash" >> $HOME/project/set.sh'
-			docker exec -it ${DOCKER_CONTAINER_NAME_01} /bin/bash -c 'echo "source $HOME/project/set.sh" >> $HOME/.bashrc'
+			docker exec -it ${DOCKER_CONTAINER_NAME_01} /bin/bash -c 'mkdir -p $(dirname '${PROFILE}')'
+			docker exec -it ${DOCKER_CONTAINER_NAME_01} /bin/bash -c 'touch '${PROFILE}
+			docker exec -it ${DOCKER_CONTAINER_NAME_01} /bin/bash -c 'echo "#!/bin/bash" >> '${PROFILE}
+			docker exec -it ${DOCKER_CONTAINER_NAME_01} /bin/bash -c 'echo "source '${PROFILE}'" >> $HOME/.bashrc'
 			docker exec -it ${DOCKER_CONTAINER_NAME_01} /bin/bash -c 'apt-get update'
 			docker exec -it ${DOCKER_CONTAINER_NAME_01} /bin/bash -c 'apt-get install -y rsync'
 			docker exec -it ${DOCKER_CONTAINER_NAME_01} /bin/bash -c 'apt-get install -y curl'
@@ -61,43 +62,48 @@ for ARG in "${@}" ; do
 			echo -------------------------------- setup rust --------------------------------
 			docker exec -it ${DOCKER_CONTAINER_NAME_01} /bin/bash -c 'apt-get install -y build-essential'
 			docker exec -it ${DOCKER_CONTAINER_NAME_01} /bin/bash -c 'curl https://sh.rustup.rs -sSf | sh -s -- -y'
-			docker exec -it ${DOCKER_CONTAINER_NAME_01} /bin/bash -c 'echo "source $HOME/.cargo/env" >> $HOME/project/set.sh'
-			docker exec -it ${DOCKER_CONTAINER_NAME_01} /bin/bash -c 'source $HOME/project/set.sh && curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -y'
+			docker exec -it ${DOCKER_CONTAINER_NAME_01} /bin/bash -c 'echo "source $HOME/.cargo/env" >> '${PROFILE}
+			docker exec -it ${DOCKER_CONTAINER_NAME_01} /bin/bash -c 'source '${PROFILE}' && curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -y'
 			echo -------------------------------- setup finish --------------------------------
 			;;
 		sync_put|put)
 			RSYNC_SRC=./
-			RSYNC_DST=${DOCKER_CONTAINER_NAME_01}:/root/project/${PROJECT}/
+			RSYNC_DST=${DOCKER_CONTAINER_NAME_01}:${PROJECT}/
 			RSYNC_COMMAND="rsync --blocking-io -e 'docker exec -i' --exclude='.git' --filter=':- .gitignore' -rltDv"
 			eval ${RSYNC_COMMAND} ${RSYNC_SRC} ${RSYNC_DST}
 			;;
 		sync_get|get)
-			RSYNC_SRC=${DOCKER_CONTAINER_NAME_01}:/root/project/${PROJECT}/
+			RSYNC_SRC=${DOCKER_CONTAINER_NAME_01}:${PROJECT}/
 			RSYNC_DST=./
 			RSYNC_COMMAND="rsync --blocking-io -e 'docker exec -i' --exclude='.git' --filter=':- .gitignore' -rltDv"
 			eval ${RSYNC_COMMAND} ${RSYNC_SRC} ${RSYNC_DST}
 			;;
 		setup_local|install)
 			echo -------------------------------- install start --------------------------------
-			docker exec -it ${DOCKER_CONTAINER_NAME_01} /bin/bash -c 'source $HOME/project/set.sh && cd $HOME/project/'${PROJECT}' && npm install'
+			docker exec -it ${DOCKER_CONTAINER_NAME_01} /bin/bash -c 'source '${PROFILE}' && cd '${PROJECT}' && npm install'
 			echo -------------------------------- install finish --------------------------------
 			;;
 		build)
 			PACKAGE=$(awk -F "\"" '/name/{ print $2 }' crate/Cargo.toml) # Cargo.tomlにnameが複数あると困る
-			docker exec -it ${DOCKER_CONTAINER_NAME_01} /bin/bash -c 'source $HOME/project/set.sh && cd $HOME/project/'${PROJECT}'/crate && wasm-pack build --dev'
-			docker exec -it ${DOCKER_CONTAINER_NAME_01} /bin/bash -c 'source $HOME/project/set.sh && cd $HOME/project/'${PROJECT}' && [ -e /node_modules/'${PACKAGE}' ] || ( cd crate/pkg && npm link )'
-			docker exec -it ${DOCKER_CONTAINER_NAME_01} /bin/bash -c 'source $HOME/project/set.sh && cd $HOME/project/'${PROJECT}' && [ -e /node_modules/'${PACKAGE}' ] || ( npm link '${PACKAGE}' )'
-			docker exec -it ${DOCKER_CONTAINER_NAME_01} /bin/bash -c 'source $HOME/project/set.sh && cd $HOME/project/'${PROJECT}' && npm run build'
+			docker exec -it ${DOCKER_CONTAINER_NAME_01} /bin/bash -c 'source '${PROFILE}' && cd '${PROJECT}'/crate && wasm-pack build --dev'
+			docker exec -it ${DOCKER_CONTAINER_NAME_01} /bin/bash -c 'source '${PROFILE}' && cd '${PROJECT}' && [ -e /node_modules/'${PACKAGE}' ] || ( cd crate/pkg && npm link )'
+			docker exec -it ${DOCKER_CONTAINER_NAME_01} /bin/bash -c 'source '${PROFILE}' && cd '${PROJECT}' && [ -e /node_modules/'${PACKAGE}' ] || ( npm link '${PACKAGE}' )'
+			docker exec -it ${DOCKER_CONTAINER_NAME_01} /bin/bash -c 'source '${PROFILE}' && cd '${PROJECT}' && npm run build'
 			;;
 		serve)
 			DOCKER_IP=$(docker inspect -f '{{.NetworkSettings.IPAddress}}' ${DOCKER_CONTAINER_NAME_01})
 			DOCKER_HOSTIP=$(docker inspect -f '{{(index (index .NetworkSettings.Ports "8080/tcp") 0).HostIp}}' ${DOCKER_CONTAINER_NAME_01})
 			DOCKER_HOSTPORT=$(docker inspect -f '{{(index (index .NetworkSettings.Ports "8080/tcp") 0).HostPort}}' ${DOCKER_CONTAINER_NAME_01})
 			echo http://${DOCKER_HOSTIP}:${DOCKER_HOSTPORT}
-			docker exec -it ${DOCKER_CONTAINER_NAME_01} /bin/bash -c 'source $HOME/project/set.sh && cd $HOME/project/'${PROJECT}' && npm run serve'
+			docker exec -it ${DOCKER_CONTAINER_NAME_01} /bin/bash -c 'source '${PROFILE}' && cd '${PROJECT}' && npm run serve'
 			;;
 		clean)
-			docker exec -it ${DOCKER_CONTAINER_NAME_01} /bin/bash -c 'rm -rf $HOME/project/'${PROJECT}'/src'
+			docker exec -it ${DOCKER_CONTAINER_NAME_01} /bin/bash -c 'source '${PROFILE}' && cd '${PROJECT}' && rm -rf src'
+			docker exec -it ${DOCKER_CONTAINER_NAME_01} /bin/bash -c 'source '${PROFILE}' && cd '${PROJECT}' && rm -rf crate/src'
+			docker exec -it ${DOCKER_CONTAINER_NAME_01} /bin/bash -c 'source '${PROFILE}' && cd '${PROJECT}' && rm -rf node_modules'
+			docker exec -it ${DOCKER_CONTAINER_NAME_01} /bin/bash -c 'source '${PROFILE}' && cd '${PROJECT}' && rm -rf crate/target'
+			docker exec -it ${DOCKER_CONTAINER_NAME_01} /bin/bash -c 'source '${PROFILE}' && cd '${PROJECT}' && rm -rf crate/pkg'
+			docker exec -it ${DOCKER_CONTAINER_NAME_01} /bin/bash -c 'source '${PROFILE}' && cd '${PROJECT}' && rm -rf dist'
 			;;
 		test)
 			;;
