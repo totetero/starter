6215a875236dd95ed70dbb81cb6dcf75166606d2
totetero <totetero@gmail.com>
2020-05-16 20:31:17 +0900

    ふがふが

--- rust_wasm/crate/src/lib.rs
+++ rust_wasm/crate/src/lib.rs
@@ -11,9 +11,11 @@ mod webgl_test_program;
 mod webgl_test_drawer;
 
 use wasm_bindgen::prelude::wasm_bindgen;
+use wasm_bindgen::JsCast;
 use wasm_bindgen::JsValue;
 use web_sys::Window;
 use web_sys::Document;
+use web_sys::Element;
 use web_sys::HtmlElement;
 use web_sys::WebGlRenderingContext;
 use webgl_get_context::webgl_get_context;
@@ -44,9 +46,10 @@ pub fn start() -> Result<(), JsValue> {
 
 	let window: Window = web_sys::window().ok_or(JsValue::from_str("failedFD28"))?;
 	let document: Document = window.document().ok_or(JsValue::from_str("failedA42A"))?;
-	let body: HtmlElement = document.body().ok_or(JsValue::from_str("failedAFCC"))?;
+	let root: Element = document.get_element_by_id("root").ok_or(JsValue::from_str("failedAFCC"))?;
+	let root: HtmlElement = root.dyn_into::<HtmlElement>()?;
 
-	let context: WebGlRenderingContext = webgl_get_context(&document, &body)?;
+	let context: WebGlRenderingContext = webgl_get_context(&document, &root)?;
 	let program: WebglTestProgram = WebglTestProgram::create(&context)?;
 	let drawer: WebglTestDrawer = WebglTestDrawer::create(&context)?;
 	drawer.test(&context, &program)?;
