805ac123d3a858b489da1c3a8f46deff7d07b472
totetero <totetero@gmail.com>
2020-05-17 17:53:49 +0900

    denoの書き方をnodeに合わせる

--- deno/src/main.ts
+++ deno/src/main.ts
@@ -3,12 +3,17 @@
 // ----------------------------------------------------------------
 // ----------------------------------------------------------------
 
-import { listenAndServe, ServerRequest, Response } from "https://deno.land/std@0.50.0/http/server.ts";
-import { serveFile } from "https://deno.land/std@0.50.0/http/file_server.ts";
+import { listenAndServe, ServerRequest, Response, } from "https://deno.land/std@0.50.0/http/server.ts";
+import { serveFile, } from "https://deno.land/std@0.50.0/http/file_server.ts";
 
 const handlerTestGet = async (req: ServerRequest, res: Response): Promise<void> => {
 	res.body = "test";
 	return req.respond(res);
+};
+
+const handlerFileGet = async (req: ServerRequest, res: Response, path: string): Promise<void> => {
+	const data: Response = await serveFile(req, path)
+	return req.respond(data);
 };
 
 const handlerError404 = async (req: ServerRequest, res: Response): Promise<void> => {
@@ -17,15 +22,26 @@ const handlerError404 = async (req: ServerRequest, res: Response): Promise<void>
 	return req.respond(res);
 };
 
-console.log("http://localhost:8080/");
+const handlerError500 = async (req: ServerRequest, res: Response, error: Error): Promise<void> => {
+	console.error(error);
+	res.status = 500;
+	res.body = "internal server error";
+	return req.respond(res);
+};
 
-listenAndServe({
-	port: 8080,
-}, async (req: ServerRequest): Promise<void> => {
+const port: number = 8080;
+console.log(`http://localhost:${port}`);
+
+listenAndServe({ port, }, async (req: ServerRequest): Promise<void> => {
 	const res: Response = {};
-	if (req.url === "/test" && req.method === "GET") { return handlerTestGet(req, res); }
-	if (req.url === "/") { return req.respond(await serveFile(req, "public/index.html")); }
-	return handlerError404(req, res);
+	try {
+		const isGet: boolean = req.method === "GET";
+		if (req.url === "/test" && isGet) { return await handlerTestGet(req, res); }
+		if (req.url === "/" && isGet) { return await handlerFileGet(req, res, "public/index.html"); }
+		return await handlerError404(req, res);
+	} catch(error) {
+		return await handlerError500(req, res, error);
+	}
 });
 
 // ----------------------------------------------------------------
