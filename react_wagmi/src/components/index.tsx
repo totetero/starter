dfaf0699f98630c61906717e85f474f455c06907
totetero <totetero@gmail.com>
2026-05-09 14:40:30 +0900

    よくわからないけどコミットしておく

--- react_wagmi/src/components/index.tsx
+++ react_wagmi/src/components/index.tsx
@@ -6,6 +6,8 @@
 import * as React from "react";
 import * as ReactRedux from "react-redux";
 import * as ReactRouterDom from "react-router-dom";
+import { WagmiConfig, createClient } from "wagmi";
+import { getDefaultProvider } from "ethers";
 import { store, } from "../redux/store";
 import Pages from "./pages";
 
@@ -20,13 +22,24 @@ type ComponentProps = unknown
 // ----------------------------------------------------------------
 // ----------------------------------------------------------------
 
+const client = createClient({
+	autoConnect: true,
+	provider: getDefaultProvider(),
+  })
+
+// ----------------------------------------------------------------
+// ----------------------------------------------------------------
+// ----------------------------------------------------------------
+
 const Component: React.FunctionComponent<ComponentProps> = (props: ComponentProps): JSX.Element => {
 	return <React.StrictMode>
-		<ReactRedux.Provider store = {store}>
-			<ReactRouterDom.HashRouter>
-				<Pages />
-			</ReactRouterDom.HashRouter>
-		</ReactRedux.Provider>
+		<WagmiConfig client={client}>
+			<ReactRedux.Provider store = {store}>
+				<ReactRouterDom.HashRouter>
+					<Pages />
+				</ReactRouterDom.HashRouter>
+			</ReactRedux.Provider>
+		</WagmiConfig>
 	</React.StrictMode>;
 };
 
